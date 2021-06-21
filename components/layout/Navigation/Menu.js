17a754b769de8810314fedeaa4827812b8235b41
juliebl <juliebernadette@gmail.com>
2021-06-21 23:02:37 +0200

    remove duplicate props

--- components/layout/Navigation/Menu.js
+++ components/layout/Navigation/Menu.js
@@ -31,34 +31,38 @@ function Menu({ menuOpen, setMenuOpen }) {
         initial="closed"
         animate="open"
         variants={variants}
-        className="flex mx-auto max-w-3xl flex-col justify-start items-center pt-40"
-        onMouseOver={() => setMenuOpen(false)}
-        onMouseOver={() => setListHover(true)}
-        onMouseOut={() => setListHover(false)}>
-        <MenuItem href="/#about">About me</MenuItem>
-        <MenuItem href="/#skills">Skills</MenuItem>
-        <MenuItem href="/projects">Projects</MenuItem>
-        <MenuItem href="/#contact">Contact</MenuItem>
-        <MenuItem href="/julie-larsen_cv.pdf">View CV</MenuItem>{' '}
-        <motion.div
-          variants={menuItemVariants}
-          className="block sm:hidden mx-auto my-6">
-          <DarkModeToggler />
-        </motion.div>
-        <motion.ul variants={variants} className="mt-6 flex">
-          <IconMenuItem href="https://github.com/juliebl">
-            <FontAwesomeIcon className="h-4" icon={['fab', 'github']} />
-            <p className="sr-only">Github Link</p>
-          </IconMenuItem>
-          <IconMenuItem href="https://www.linkedin.com/in/julie-larsen-364681190/">
-            <FontAwesomeIcon className="h-4" icon={['fab', 'linkedin']} />
-            <p className="sr-only">Linkedin Link</p>
-          </IconMenuItem>
-          <IconMenuItem href="https://discordapp.com/users/applecake#5502">
-            <FontAwesomeIcon className="h-4" icon={['fab', 'discord']} />
-            <p className="sr-only">Discord User Link</p>
-          </IconMenuItem>
-        </motion.ul>{' '}
+        className="pt-40">
+        <div
+          className="mx-auto max-w-lg text-center"
+          onMouseOver={() => {
+            setListHover(true);
+          }}
+          onMouseOut={() => setListHover(false)}>
+          <MenuItem href="/#about">About me</MenuItem>
+          <MenuItem href="/#skills">Skills</MenuItem>
+          <MenuItem href="/projects">Projects</MenuItem>
+          <MenuItem href="/#contact">Contact</MenuItem>
+          <MenuItem href="/julie-larsen_cv.pdf">View CV</MenuItem>{' '}
+          <motion.div
+            variants={menuItemVariants}
+            className="block sm:hidden mx-auto my-6">
+            <DarkModeToggler />
+          </motion.div>
+          <motion.ul variants={variants} className="mt-6 flex justify-center">
+            <IconMenuItem href="https://github.com/juliebl">
+              <FontAwesomeIcon className="h-4" icon={['fab', 'github']} />
+              <p className="sr-only">Github Link</p>
+            </IconMenuItem>
+            <IconMenuItem href="https://www.linkedin.com/in/julie-larsen-364681190/">
+              <FontAwesomeIcon className="h-4" icon={['fab', 'linkedin']} />
+              <p className="sr-only">Linkedin Link</p>
+            </IconMenuItem>
+            <IconMenuItem href="https://discordapp.com/users/applecake#5502">
+              <FontAwesomeIcon className="h-4" icon={['fab', 'discord']} />
+              <p className="sr-only">Discord User Link</p>
+            </IconMenuItem>
+          </motion.ul>
+        </div>
       </motion.ul>
     </motion.div>
   );
