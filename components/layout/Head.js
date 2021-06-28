c8e3f27635eaf9f4ac1c07f6046855fba56e4f04
juliebl <juliebernadette@gmail.com>
2021-06-28 15:10:22 +0200

    add meta tags

--- components/layout/Head.js
+++ components/layout/Head.js
@@ -6,9 +6,40 @@ function Head({ title = '' }) {
     <NextHead>
       <title>
         {title}
-        {title ? ' | ' : ''}Julie Larsen
+        {title ? ' | ' : ''}Julie Larsen - Front-end developer
       </title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="keywords"
+        content="portfolio, front end development, noroff, stavanger, sandnes"
+      />
+      <meta name="author" content="Julie Larsen" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="title" content="Julie Larsen - Front-end developer" />
+      <meta
+        name="description"
+        content="Portfolio of front end developer Julie Larsen who is located in Stavanger"
+      />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.julielarsen.dev/" />
+      <meta property="og:title" content="Julie Larsen - Front-end developer" />
+      <meta
+        property="og:description"
+        content="Portfolio of front end developer Julie Larsen who is located in Stavanger"
+      />
+      <meta property="og:image" content="/screenshot_website.png" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://www.julielarsen.dev/" />
+      <meta
+        property="twitter:title"
+        content="Julie Larsen - Front-end developer"
+      />
+      <meta
+        property="twitter:description"
+        content="Portfolio of front end developer Julie Larsen who is located in Stavanger"
+      />
+      <meta property="twitter:image" content="/screenshot_website.png" />
       <link rel="icon" href="/favicon.ico" />
       <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </NextHead>
